d7d8ec0a98173c869f5d214a23e9c3318a2064f5
alphacentauri07 <valay936khobragade@gmail.com>
2025-01-17 01:05:11 +0530

    loginsignup,updateprofile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,18 @@ import Home from "./component/Home/Home.js"
 import ProductDetails from './component/Product/ProductDetails.js';
 import Products from "./component/Product/Products.js"
 import Search from "./component/Product/Search.js"
+import LoginSignUp from './component/User/LoginSignUp.js';
+import store from "./store.js"
+import { loadUser } from './actions/userAction.js';
+import UserOptions from "./component/Layout/Header/UserOptions.js"
+import { useSelector } from 'react-redux';
+import Profile from "./component/User/Profile.js"
+import ProtectedRoute from './component/Route/ProtectedRoute.js';
+import UpdateProfile from "./component/User/UpdateProfile.js"
 
 function App() {
+
+const { isAuthenticated,user} = useSelector((state)=>state.user)
 
   useEffect(() => {
     WebFont.load({
@@ -17,25 +27,34 @@ function App() {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
+      
+     store.dispatch(loadUser());
 
   }, []);
 
 
   return (
     <Router>
-      <Header/>
+      <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Routes>
-      <Route path = "/" element={<Home/>}/>
-      <Route path = "/product/:id" element={<ProductDetails/>}/>
-      <Route path = "/products" element={<Products/>}/>
-      <Route path = "/products/:keyword" element={<Products/>}/>
-
-      <Route path = "/search" element={<Search/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:keyword" element={<Products />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<LoginSignUp />} />
+        
+        /* Protected Route */
+        <Route element={<ProtectedRoute />}>
+          <Route path="/account" element={<Profile />} />
+          <Route path="/me/update" element={<UpdateProfile/>}/>
+        </Route>
       </Routes>
-
-      <Footer/>
+      <Footer />
     </Router>
   );
+  
 }
 
 export default App;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {thunk} from "redux-thunk";
 import { productDetailsReducer, productsReducer } from "./reducers/productReducer";
+import { profileReducer, userReducer } from "./reducers/userReducer";
+
+
 
 
 const store = configureStore({
   reducer: {
    products:productsReducer,
    productDetails:productDetailsReducer,
+   user:userReducer,
+   profile:profileReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(thunk), // Add custom middleware like thunk
